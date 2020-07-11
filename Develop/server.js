9006fa31c820a6abda8df6d53a3929ba4c22d148
probison <parker.robison@hotmail.com>
2020-07-10 22:25:59 -0600

    started server

---
+++ Develop/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const { notes } = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
