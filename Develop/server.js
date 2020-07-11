4145caa527ab1bdf3b5e2270da44a6cd955c12f1
probison <parker.robison@hotmail.com>
2020-07-10 23:55:55 -0600

    heroku again

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const { notes } = require('./db/db.json');
 
+//note so I can save 
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
