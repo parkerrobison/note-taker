e9eef81f439d903073dfb0823e041ea9631e7216
probison <parker.robison@hotmail.com>
2020-07-11 21:39:14 -0600

    got add note function and delete function working

--- server.js
+++ server.js
@@ -1,28 +1,71 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json');
 
-
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.static('public'));
+const notes  = require('./db/db.json');
+let id = notes.length + 1;
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
 
+app.post('/api/notes', (req, res) => {
+    req.body.id = id++;
+    notes.forEach(note => {
+        if(req.body.id === note.id) {
+            req.body.id++
+        }
+    })
+
+    notes.push(req.body)
+    fs.writeFile('./db/db.json',JSON.stringify(notes), function(error) {
+            if (error) throw error;
+
+            res.json(notes);
+        }
+    )
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+app.delete('/api/notes/:id', (req, res) => {
+    console.log(req.params.id);
+    for (let i=0; i<notes.length; i++) {
+        if(notes[i].id === parseInt(req.params.id)) {
+            notes.splice(i, 1)
+        }
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(notes), function(error) {
+        if (error) throw error;
+        res.json(notes);
+        })
+})
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+
+// The application should have a db.json file on the back end 
+// that will be used to store and retrieve notes using the fs module.
+// The following HTML routes should be created:
+// GET /notes should return the notes.html file.
+// GET * should return the index.html file.
+// The following API routes should be created:
+// GET /api/notes should read the db.json file 
+// and return all saved notes as JSON.
+// POST /api/notes should receive a new note to save on the request body,
+//  add it to the db.json file, and then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved 
+// (look into npm packages that could do this for you).
